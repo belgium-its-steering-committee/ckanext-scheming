8555f982a89a49ad3d127dac8449082be1f6710e
Bart Saelen <bart.saelen@geosolutions.be>
2022-03-15 10:41:14 +0100

    Scheming split help text by link (#15)
    scheming_split_help_text_by_link

--- ckanext/scheming/helpers.py
+++ ckanext/scheming/helpers.py
@@ -342,3 +342,21 @@ def scheming_display_json_value(value, indent=2):
         return json.dumps(value, indent=indent, sort_keys=True)
     except (TypeError, ValueError):
         return value
+
+
+def scheming_split_help_text_by_link(help_text):
+    """
+    Split help text in different parts by looking for '<a>' tags
+    :param help_text: input help text
+    :return: list of parts
+    """
+    parts_list = []
+    if "<a>" in help_text:
+        parts = help_text.split("<a>")
+        parts2 = parts[1].split("</a>")
+        parts_list.append({"text": parts[0], "link": False})
+        parts_list.append({"text": parts2[0], "link": True})
+        parts_list.append({"text": parts2[1], "link": False})
+    else:
+        parts_list.append({"text": help_text, "link": False})
+    return parts_list
