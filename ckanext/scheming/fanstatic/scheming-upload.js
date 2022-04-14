247394dd6dae341f399de727ae1a12230e58d6b2
Bart Saelen <bart.saelen@geosolutions.be>
2022-04-14 10:20:46 +0200

    upload refactor

--- ckanext/scheming/fanstatic/scheming-upload.js
+++ ckanext/scheming/fanstatic/scheming-upload.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
-    $( "div[data-module-field_upload|='rtti_upload_doc']>div.form-group>label" ).text( $("div[data-module-field_upload|='rtti_upload_doc']" ).attr("data-module-upload_label") );
-    $( "div[data-module-field_upload|='srti_upload_doc']>div.form-group>label" ).text( $("div[data-module-field_upload|='srti_upload_doc']" ).attr("data-module-upload_label") );
-    $( "div[data-module-field_upload|='sstp_upload_doc']>div.form-group>label" ).text( $("div[data-module-field_upload|='sstp_upload_doc']" ).attr("data-module-upload_label") );
-    alert( $("div[data-module-field_upload|='srti_upload_doc']" ).attr("data-module-upload_label") )
-});
+    setTimeout(function(){
+        $( "div[data-module-field_upload|='rtti_upload_doc']>div.form-group>label" ).text( $("div[data-module-field_upload|='rtti_upload_doc']" ).attr("data-module-upload_label") );
+        $( "div[data-module-field_upload|='srti_upload_doc']>div.form-group>label" ).text( $("div[data-module-field_upload|='srti_upload_doc']" ).attr("data-module-upload_label") );
+        $( "div[data-module-field_upload|='sstp_upload_doc']>div.form-group>label" ).text( $("div[data-module-field_upload|='sstp_upload_doc']" ).attr("data-module-upload_label") );
+        alert($("div[data-module-field_upload|='srti_upload_doc']" ).attr("data-module-upload_label"))
+    },2000);
+  });
