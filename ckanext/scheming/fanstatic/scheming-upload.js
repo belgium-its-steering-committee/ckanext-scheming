f74b5c8883cb52de329fc724b500eccf4226bc08
Bart Saelen <bart.saelen@geosolutions.be>
2022-04-14 10:21:39 +0200

    upload refactor

--- ckanext/scheming/fanstatic/scheming-upload.js
+++ ckanext/scheming/fanstatic/scheming-upload.js
@@ -3,6 +3,5 @@ $(document).ready(function(){
         $( "div[data-module-field_upload|='rtti_upload_doc']>div.form-group>label" ).text( $("div[data-module-field_upload|='rtti_upload_doc']" ).attr("data-module-upload_label") );
         $( "div[data-module-field_upload|='srti_upload_doc']>div.form-group>label" ).text( $("div[data-module-field_upload|='srti_upload_doc']" ).attr("data-module-upload_label") );
         $( "div[data-module-field_upload|='sstp_upload_doc']>div.form-group>label" ).text( $("div[data-module-field_upload|='sstp_upload_doc']" ).attr("data-module-upload_label") );
-        alert($("div[data-module-field_upload|='srti_upload_doc']" ).attr("data-module-upload_label"))
-    },2000);
+    },1000);
   });
